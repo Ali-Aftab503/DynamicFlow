39cfbf7207c4bc9c0b49083c2a4e1c4d9debf4f8
Ali Aftab <aliaftab3503@gmail.com>
2025-11-22 20:47:31 +0500

    Remove next-auth completely

--- components/integrations/GoogleDrivePicker.tsx
+++ components/integrations/GoogleDrivePicker.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { useSession, signIn } from "next-auth/react";
 import { FolderOpen } from "lucide-react";
 
 interface GoogleDrivePickerProps {
@@ -14,59 +13,22 @@ export function GoogleDrivePicker({
   cardId,
   onFileSelected,
 }: GoogleDrivePickerProps) {
-  const { data: session } = useSession();
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleConnect = async () => {
-    await signIn("google");
-  };
-
-  const openPicker = () => {
-    if (!session?.accessToken) {
-      handleConnect();
-      return;
-    }
-
-    setIsLoading(true);
-
-    // Load Google Picker API
-    const script = document.createElement("script");
-    script.src = "https://apis.google.com/js/api.js";
-    script.onload = () => {
-      // @ts-ignore
-      gapi.load("picker", () => {
-        // @ts-ignore
-        const picker = new google.picker.PickerBuilder()
-          .addView(
-            // @ts-ignore
-            new google.picker.DocsView(google.picker.ViewId.DOCS)
-          )
-          .setOAuthToken(session.accessToken)
-          .setDeveloperKey(process.env.NEXT_PUBLIC_GOOGLE_API_KEY!)
-          .setCallback((data: any) => {
-            if (data.action === "picked") {
-              const file = data.docs[0];
-              onFileSelected(file);
-            }
-            setIsLoading(false);
-          })
-          .build();
-        picker.setVisible(true);
-      });
-    };
-    document.body.appendChild(script);
+  const handleConnect = () => {
+    alert("Google Drive integration requires additional setup. For now, use the file upload feature.");
   };
 
   return (
     <Button
       type="button"
       variant="outline"
-      onClick={openPicker}
+      onClick={handleConnect}
       disabled={isLoading}
       className="w-full"
     >
       <FolderOpen className="h-4 w-4 mr-2" />
-      {session ? "Attach from Google Drive" : "Connect Google Drive"}
+      Connect Google Drive
     </Button>
   );
 }

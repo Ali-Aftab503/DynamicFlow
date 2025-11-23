b207f730a21ad15dc9761f58ef5a09f4f34d6277
Ali Aftab <aliaftab3503@gmail.com>
2025-11-24 02:59:02 +0500

    package.json updated  to use the standard prisma generate command for postinstall

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   turbopack: {},
   serverExternalPackages: ['@prisma/client'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-    }
-    return config;
-  },
+
 };
 
 export default nextConfig;
